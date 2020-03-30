4d26391b2d606462fbe456dc83c4048de6f770ff
sravaisridhara <sravanisridhara94@gmail.com>
2020-03-30 12:47:56 -0400

    Config changes

--- src/main/java/com/example/ConfigServer/ConfigServerApplication.java
+++ src/main/java/com/example/ConfigServer/ConfigServerApplication.java
@@ -5,10 +5,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
 @EnableConfigServer
+@EnableEurekaClient
 public class ConfigServerApplication {
 
 	public static void main(String[] args) {
